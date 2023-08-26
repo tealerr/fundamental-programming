8d7e3de77a04b32807cda6535f23286aaac17106
teeramate <teeramate@ascendcorp.com>
2023-08-26 14:01:04 +0700

    add logic operator

---
+++ primitive-datatype/Java/logic.java
@@ -0,0 +1,14 @@
+public class logic{
+    public static void main(String[] args) {
+        //Logic AND
+         int a = 2;
+         int b = 3;
+
+         if (a>=1&&b<=2){
+            System.out.println("Hello World")
+         }
+        
+
+
+    }
+}
