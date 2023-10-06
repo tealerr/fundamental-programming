110378afff89b8b5246b61d9416f39df775bae11
Teeramet.K <theetitle@gmail.com>
2023-10-06 11:27:36 +0700

    build c++ project

---
+++ C++/build/CMakeFiles/myCmakeCpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myCmakeCpp.dir/condition.cpp.o"
+  "CMakeFiles/myCmakeCpp.dir/condition.cpp.o.d"
+  "CMakeFiles/myCmakeCpp.dir/main.cpp.o"
+  "CMakeFiles/myCmakeCpp.dir/main.cpp.o.d"
+  "myCmakeCpp"
+  "myCmakeCpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myCmakeCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
