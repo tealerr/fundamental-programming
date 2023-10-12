7f4b9b70c96f4cf18e954a8098635189e97772b3
Teeramet.K <theetitle@gmail.com>
2023-10-12 10:40:16 +0700

    bypass .cpp scan

--- Clojure/project.clj
+++ Clojure/project.clj
@@ -1,7 +1,7 @@
 (defproject your-project-name "1.0.0"
   :plugins [[jonase/eastwood "0.3.13"]
-            [lein-kibit "0.1.8"]
-            [lein-ancient "0.6.15"]
-            [lein-cloverage "1.1.2"]
-            [lein-nvd "1.4.0"]]
-  :dependencies [[clj-kondo "RELEASE"]])
+          [lein-kibit "0.1.8"]
+          [lein-ancient "0.6.15"]
+          [lein-cloverage "1.1.2"]
+          [lein-nvd "1.4.0"]]
+:dependencies [[clj-kondo "RELEASE"]])
